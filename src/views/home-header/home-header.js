812ab8d832f6ed921055fd00296d68af58cb4a1b
andefine <1939866551@qq.com>
2019-04-18 00:47:01 +0800

    feat[search]: add search

--- src/views/home-header/home-header.js
+++ src/views/home-header/home-header.js
@@ -3,6 +3,7 @@ import './home-header.scss'
 
 import Logo from  './logo/logo'
 import BackForward from './back-forward/back-forward'
+import Search from './search/search'
 
 export default class HomeHeader extends Component {
   render () {
@@ -11,6 +12,7 @@ export default class HomeHeader extends Component {
         <div className="home-header__left">
           <Logo></Logo>
           <BackForward className="home-header__bf"></BackForward>
+          <Search className="home-header__search"></Search>
         </div>
         <div className="home-header__right"></div>
       </header>
