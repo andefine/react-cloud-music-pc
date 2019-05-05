23a4fb2541ae9d0ad1980c37708b57ec56b59737
andefine <1939866551@qq.com>
2019-05-05 23:00:08 +0800

    feat: add private content

--- src/views/recommend/private-content/private-content.js
+++ src/views/recommend/private-content/private-content.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './private-content.scss'
+import PrivateContentCard from '@/components/private-content-card/private-content-card'
 
-const PrivateContent = () => {
+const PrivateContent = ({ privateContents }) => {
   return (
     <div className="private-content">
-      <div className="private-content__item"></div>
-      <div className="private-content__item"></div>
-      <div className="private-content__item"></div>
+      {
+        privateContents.map(({ id, copywriter, sPicUrl }) => {
+          return (
+            <PrivateContentCard
+              key={id}
+              copywriter={copywriter}
+              sPicUrl={sPicUrl}
+            ></PrivateContentCard>
+          )
+        })
+      }
     </div>
   )
 }
 
-export default PrivateContent
+const mapStateToProps = ({ recommend }) => ({
+  privateContents: recommend.privateContents
+})
+
+export default connect(
+  mapStateToProps
+)(PrivateContent)
