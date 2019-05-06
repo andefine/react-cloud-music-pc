dec8f0db4560a76127e9a99189b4d8b6639339f5
andefine <1939866551@qq.com>
2019-05-06 21:55:40 +0800

    chore: add latest-music files

--- src/views/recommend/recommend.js
+++ src/views/recommend/recommend.js
@@ -7,6 +7,7 @@ import TopSwiper from '@/components/top-swiper/top-swiper'
 import CardTitle from '@/components/card-list-title/card-list-title'
 import RecommendPlaylist from './recommend-playlist/recommend-playlist'
 import PrivateContent from './private-content/private-content'
+import LatestMusic from './latest-music/latest-music'
 
 function Recommend ({ banners }) {
   return (
@@ -22,6 +23,8 @@ function Recommend ({ banners }) {
       <RecommendPlaylist></RecommendPlaylist>
       <CardTitle className="recommend__only-title" title="独家放送"></CardTitle>
       <PrivateContent></PrivateContent>
+      <CardTitle title="最新音乐"></CardTitle>
+      <LatestMusic></LatestMusic>
     </div>
   )
 }
