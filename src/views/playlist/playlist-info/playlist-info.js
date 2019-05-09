1f1790c5d19923f7e100705ec82745f80c282624
andefine <1939866551@qq.com>
2019-05-10 01:00:41 +0800

    view[playlist-info]: add some htlml

--- src/views/playlist/playlist-info/playlist-info.js
+++ src/views/playlist/playlist-info/playlist-info.js
@@ -3,15 +3,74 @@ import React from 'react'
 import './playlist-info.scss'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import IconButton from '@/components/icon-button/icon-button'
+
+/**
+ * 将个位数添零
+ *
+ * @param {*} num
+ * @returns {String}
+ */
+const addZero = (num) => {
+  if (num > 9) {
+    return '' + num
+  }
+  return '0' + num
+}
+
+/**
+ * 将时间戳生成 年月日 字符串。
+ *
+ * @param {*} stamp 时间戳
+ * @returns {String} 年月日 字符串。格式 xxxx-xx-xx
+ */
+const formatYearMonthDate = (stamp) => {
+  const date = new Date(stamp)
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const dateNum = date.getDate()
+  return `${year}-${addZero(month)}-${addZero(dateNum)}`
+}
 
 const PlaylistInfo = ({ playlist }) => {
   const {
-    name
+    coverImgUrl,
+    name,
+    creator,
+    createTime
   } = playlist
+
+  const { avatarUrl, nickname } = creator
   
   return (
     <div className="playlist-info">
-      {name}
+      <img src={coverImgUrl} alt="" className="playlist-info__cover-img"/>
+
+      <div className="playlist-info__right">
+
+        <div className="playlist-info__title">
+          <div className="playlist-info__title-tag">歌单</div>
+          {name}
+        </div>
+
+        <div className="playlist-info__creator">
+          <div className="playlist-info__avatar">
+            <img src={avatarUrl} alt="" className="playlist-info__avatar-img"/>
+          </div>
+          <span className="playlist-info__nickname">{nickname}</span>
+          <span className="playlist-info__create-time">
+            {formatYearMonthDate(createTime)}创建
+          </span>
+        </div>
+
+        <div className="playlist-info__btns">
+          <IconButton icon="yibaocun">已收藏</IconButton>
+          <IconButton icon="share">分享</IconButton>
+          <IconButton icon="download">下载全部</IconButton>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
@@ -23,6 +82,7 @@ PlaylistInfo.propTypes = {
     trackCount: PropTypes.number.isRequired,
     playCount: PropTypes.number.isRequired,
     creator: PropTypes.shape({
+      avatarUrl: PropTypes.string.isRequired,
       nickname: PropTypes.string.isRequired
     }),
     createTime: PropTypes.number.isRequired,
